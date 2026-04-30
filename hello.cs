120a9aad73d2df3b75610057e6fda796f42445d3
DevCrisso <crisologo.aguilar.27@unsch.edu.pe>
2026-04-29 23:05:22 -0500

    I need to change my project and start right

--- hello.cs
+++
@@ -1,5 +0,0 @@
-using System;
-Console.WriteLine("Escribe tu nombre de manera adecuada");
-
-string myFirstVariable=Console.ReadLine(); 
-Console.WriteLine($"Your name is {myFirstVariable}");
--- hello_world.py
+++
@@ -1,7 +0,0 @@
-print("Hello world")
-a=5
-b=6
-if a==b:
-    print("Felicidades")
-else:
-    print("fallaste")
